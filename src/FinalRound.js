0be4ddc26df38cf4cb5886bf235fc7ab032a652d
Michael Schneider <michaelschneider@Michaels-MacBook-Pro.local>
2019-09-10 16:39:28 -0600

    Add yellow highlight to current player and new file for FinalRound.js
    Co-authored-by: Foster Taylor <fosterataylor@yahoo.com>

---
+++ src/FinalRound.js
@@ -0,0 +1,23 @@
+import Clue from "./Clue";
+import domUpdates from "./domUpdates";
+import Round from "./Round";
+
+class FinalRound extends Round {
+  constructor(data, game, fourCategories, currentRound) {
+    super(data, game, fourCategories, currentRound);
+  }
+
+  takeWagers() {
+
+  }
+
+  getAnswers() {
+
+  }
+
+  finalScores() {
+
+  }
+}
+
+export default FinalRound;
